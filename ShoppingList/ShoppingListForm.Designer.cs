e1af65f058269aaf877a518cea1013478b61fa50
Anna <Anna@ANNA-VAIO>
2018-06-11 18:32:01 +0300

    Add project files.

---
+++ ShoppingList/ShoppingListForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ShoppingList
+{
+    partial class ShoppingListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.upButton = new System.Windows.Forms.Button();
+            this.downButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.addButton.Enabled = false;
+            this.addButton.Location = new System.Drawing.Point(149, 249);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.deleteButton.Enabled = false;
+            this.deleteButton.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.deleteButton.Location = new System.Drawing.Point(12, 275);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(212, 23);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(12, 249);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(131, 20);
+            this.textBox.TabIndex = 3;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // checkedListBox
+            // 
+            this.checkedListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox.FormattingEnabled = true;
+            this.checkedListBox.Location = new System.Drawing.Point(12, 14);
+            this.checkedListBox.Name = "checkedListBox";
+            this.checkedListBox.Size = new System.Drawing.Size(293, 229);
+            this.checkedListBox.TabIndex = 4;
+            this.checkedListBox.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // upButton
+            // 
+            this.upButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.upButton.Enabled = false;
+            this.upButton.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.upButton.Location = new System.Drawing.Point(230, 249);
+            this.upButton.Name = "upButton";
+            this.upButton.Size = new System.Drawing.Size(75, 23);
+            this.upButton.TabIndex = 5;
+            this.upButton.Text = "Up";
+            this.upButton.UseVisualStyleBackColor = true;
+            this.upButton.Click += new System.EventHandler(this.upButton_Click);
+            // 
+            // downButton
+            // 
+            this.downButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.downButton.Enabled = false;
+            this.downButton.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.downButton.Location = new System.Drawing.Point(230, 275);
+            this.downButton.Name = "downButton";
+            this.downButton.Size = new System.Drawing.Size(75, 23);
+            this.downButton.TabIndex = 6;
+            this.downButton.Text = "Down";
+            this.downButton.UseVisualStyleBackColor = true;
+            this.downButton.Click += new System.EventHandler(this.downButton_Click);
+            // 
+            // ShoppingListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 304);
+            this.Controls.Add(this.downButton);
+            this.Controls.Add(this.upButton);
+            this.Controls.Add(this.checkedListBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Name = "ShoppingListForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.Button upButton;
+        private System.Windows.Forms.Button downButton;
+    }
+}
+
---
+++ ShoppingList/ShoppingListForm.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ShoppingList
+{
+    public partial class ShoppingListForm : Form
+    {
+        private string name;
+
+        public ShoppingListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            checkedListBox.Items.Add(textBox.Text);
+            textBox.Clear();
+            textBox.Focus();
+            addButton.Enabled = false;
+        }
+
+        private void checkedListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (checkedListBox.SelectedItems.Count != 0)
+            {
+                addButton.Enabled = false;
+                deleteButton.Enabled = true;
+                upButton.Enabled = true;
+                downButton.Enabled = true;
+            }
+        }
+
+        private void downButton_Click(object sender, EventArgs e)
+        {
+            int highestIndex = 0;
+            for (int i = 0; i < checkedListBox.Items.Count; i++)
+            {
+                if (!checkedListBox.SelectedIndices.Contains(i))
+                {
+                    highestIndex = i;
+                }
+            }
+
+            int select = checkedListBox.SelectedItems.Count;
+            for (int i = select - 1; i >= 0; i--)
+            {
+                if (checkedListBox.SelectedIndices[i] < checkedListBox.Items.Count - 1)
+                {
+                    int index = checkedListBox.SelectedIndices[i] + 2;
+                    if (checkedListBox.CheckedItems.Count > 0)
+                    {
+                        checkedListBox.Items.Insert(index, checkedListBox.SelectedItems[i]);
+                        checkedListBox.Items.RemoveAt(index - 2);
+                        checkedListBox.SelectedItem = checkedListBox.Items[index - 1];
+                        checkedListBox.SetItemChecked(index - 1, true);
+                    }
+                    else
+                    {
+                        checkedListBox.Items.Insert(index, checkedListBox.SelectedItems[i]);
+                        checkedListBox.Items.RemoveAt(index - 2);
+                        checkedListBox.SelectedItem = checkedListBox.Items[index - 1];
+                    }
+                }
+            }
+            checkedListBox.EndUpdate();
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox.Items.Count > 0)
+            {
+                checkedListBox.Items.RemoveAt(checkedListBox.SelectedIndex);
+            }
+            deleteButton.Enabled = false;
+            upButton.Enabled = false;
+            downButton.Enabled = false;
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox.Text != null)
+            {
+                if (checkedListBox.Items.Contains(textBox.Text))
+                {
+                    name = textBox.Text;
+                    addButton.Enabled = false;
+                }
+                else
+                {
+                    name = textBox.Text;
+                    textBox.Text = textBox.Text.Trim();
+                    addButton.Enabled = true;
+                }
+            }
+        }
+
+        private void upButton_Click(object sender, EventArgs e)
+        {
+            int lowestIndex = checkedListBox.Items.Count - 1;
+
+            for ( int i = lowestIndex; i >= 0; i --)
+            {
+                if (!checkedListBox.SelectedIndices.Contains(i))
+                {
+                    lowestIndex = i;
+                }
+            }
+            checkedListBox.BeginUpdate();
+            int select = checkedListBox.SelectedItems.Count;
+
+            for (int i = 0; i < select; i++)
+            {
+                if (checkedListBox.SelectedIndices[i] > 0 )
+                {
+                    int index = checkedListBox.SelectedIndices[i] - 1;
+                    if (checkedListBox.CheckedItems.Count > 0)
+                    {
+                        checkedListBox.Items.Insert(index, checkedListBox.SelectedItems[i]);
+                        checkedListBox.Items.RemoveAt(index + 2);
+                        checkedListBox.SelectedItem = checkedListBox.Items[index];
+                        checkedListBox.SetItemChecked(index, true);
+                    }
+                    else
+                    {
+                        checkedListBox.Items.Insert(index, checkedListBox.SelectedItems[i]);
+                        checkedListBox.Items.RemoveAt(index + 2);
+                        checkedListBox.SelectedItem = checkedListBox.Items[index];
+                    }  
+                }
+            }
+            checkedListBox.EndUpdate();
+        }
+    }
+}
